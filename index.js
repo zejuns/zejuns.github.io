37621cbcd88641cb07c398902c129729f9fb4809
zej-h <114798179+zej-h@users.noreply.github.com>
2023-07-29 04:21:03 +0800

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-var links = document.querySelectorAll('a[href^="#"]');
-links.forEach(function(link) {
-  link.addEventListener('click', function(event) {
-    event.preventDefault();
-    var target = document.querySelector(this.getAttribute('href'));
-    var offset = target.offsetTop - 44;
-    window.scrollTo({ top: offset, behavior: 'smooth' });
-  });
-});
