5bf94b8dacbc184e3dcaf586c4c528160482e766
zej-h <hzj020117@outlook.com>
2024-03-04 18:17:21 +0800

    1

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
+;(function($) {
+	$(window).load(function() { // makes sure the whole site is loaded
+		$('.loader-xbox').fadeOut(); // will first fade out the loading animation
+		$('#preloader').delay(150).fadeOut('slow'); // will fade out the white DIV that covers the website.
+		$('body').delay(150).css({'overflow':'visible'})
+	})
+   
+})(jQuery)
+
 $(document).ready(function() {
 	// Header Scroll
 	$(window).on('scroll', function() {
