ad9162fb301450ff781621c31114214dbe06b3d9
zejuns <114798179+zejuns@users.noreply.github.com>
2024-03-16 19:07:50 +0800

    1

--- js/main.js
+++ js/main.js
@@ -2,8 +2,8 @@
     $(window).load(function() {
         // makes sure the whole site is loaded
         $('.loader-xbox').fadeOut(); // will first fade out the loading animation
-        $('#preloader').delay(150).fadeOut('slow'); // will fade out the white DIV that covers the website
-        $('body').delay(150).css({'overflow':'visible'});
+        $('#preloader').fadeOut('slow'); // will fade out the white DIV that covers the website
+        $('body').css({'overflow':'visible'});
 
         // Mobile Navigation
         $('.nav-toggle').on('click', function() {
