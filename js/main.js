a6b08b3d95e95453126e28468d7b159c7afaf257
zejuns <114798179+zejuns@users.noreply.github.com>
2024-09-10 18:20:49 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,67 +3,58 @@ document.addEventListener('DOMContentLoaded', function() {
     var dropdownMenus = document.querySelectorAll('.dropdown-menu');
     var timeoutId;
 
-    function showDropdown(index) {
-        // 显示当前主菜单对应的子菜单
-        var subMenu = navigationLinks[index].nextElementSibling;
-        if (subMenu) {
-            subMenu.style.display = 'block';
-        }
-    }
-
-    function hideDropdown() {
-        dropdownMenus.forEach(function(menu) {
-            menu.style.display = 'none';
-        });
-    }
-
     navigationLinks.forEach(function(link, index) {
         link.addEventListener('mouseover', function() {
-            if (window.innerWidth > 768) { // 仅在视口宽度大于768px时显示下拉框
-                clearTimeout(timeoutId); // 清除之前的延迟隐藏计时器
-                hideDropdown();
-                showDropdown(index);
+            clearTimeout(timeoutId); // 清除之前的延迟隐藏计时器
+
+            // 隐藏其他子菜单
+            dropdownMenus.forEach(function(menu) {
+                menu.style.display = 'none';
+            });
+
+            // 显示当前主菜单对应的子菜单
+            var subMenu = link.nextElementSibling;
+            if (subMenu) {
+                subMenu.style.display = 'block';
             }
         });
 
         link.addEventListener('mouseout', function() {
-            if (window.innerWidth > 768) { // 仅在视口宽度大于768px时隐藏下拉框
-                timeoutId = setTimeout(hideDropdown, 500);
-            }
+            timeoutId = setTimeout(function() {
+                dropdownMenus.forEach(function(menu) {
+                    menu.style.display = 'none';
+                });
+            }, 500);
         });
     });
 
     dropdownMenus.forEach(function(menu) {
         menu.addEventListener('mouseover', function() {
-            if (window.innerWidth > 768) { // 仅在视口宽度大于768px时显示下拉框
-                menu.style.display = 'block';
-            }
+            menu.style.display = 'block';
         });
 
         menu.addEventListener('mouseout', function() {
-            if (window.innerWidth > 768) { // 仅在视口宽度大于768px时隐藏下拉框
-                menu.style.display = 'none';
-            }
+            menu.style.display = 'none';
         });
     });
 
-    // Gitalk 初始化代码
     const gitalk = new Gitalk({
         clientID: '2658e1c2a15202f4ea1a',
         clientSecret: 'efe03ae68db5b4aef7fa72a3aa7bbf249a143383',
-        repo: 'zejuns.github.io',
+        repo: 'zejuns.github.io',      // The repository of store comments,
         owner: 'zejuns',
         admin: ['zejuns'],
-        id: location.pathname,
-        distractionFreeMode: false
+        id: location.pathname,      // Ensure uniqueness and length less than 50
+        distractionFreeMode: false  // Facebook-like distraction free mode
     });
-    gitalk.render('gitalk-container');
+    gitalk.render('gitalk-container')
 });
 
 (function($) {
     $(window).load(function() {
-        $('.loader-xbox').fadeOut();
-        $('#preloader').fadeOut('slow');
+        // makes sure the whole site is loaded
+        $('.loader-xbox').fadeOut(); // will first fade out the loading animation
+        $('#preloader').fadeOut('slow'); // will fade out the white DIV that covers the website
         $('body').css({'overflow':'visible'});
 
         // Mobile Navigation
@@ -71,6 +62,7 @@ document.addEventListener('DOMContentLoaded', function() {
             $(this).toggleClass('close-nav');
             $('nav[role="navigation"]').toggleClass('open');
             
+            // 切换全屏菜单时禁止或恢复页面滚动
             if ($('nav[role="navigation"]').hasClass('open')) {
                 $('.fullscreen-menu').css('display', 'block');
                 $('body').css('overflow', 'hidden');
@@ -86,6 +78,7 @@ document.addEventListener('DOMContentLoaded', function() {
             $('.nav-toggle').toggleClass('close-nav');
             $('nav[role="navigation"]').toggleClass('open');
 
+            // 关闭全屏菜单时恢复页面滚动
             if (!$('nav[role="navigation"]').hasClass('open')) {
                 $('.fullscreen-menu').css('display', 'none');
                 $('body').css('overflow', 'auto');
@@ -95,6 +88,7 @@ document.addEventListener('DOMContentLoaded', function() {
 })(jQuery);
 
 $(document).ready(function() {
+    // Header Scroll
     $(window).on('scroll', function() {
         var scroll = $(window).scrollTop();
 
